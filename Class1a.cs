c19078e2ca7570915a80e0cc740d003dfaab3c00
BRODOSZPAN <BRODOSZPAN@LAPTOP-KEO4J087>
2023-01-29 21:31:20 +0100

    Dodaj pliki projektów.

---
+++ Class1a.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace kółko_i_krzyżyk
+{
+    public class Class1a
+    {
+    }
+}
---
+++ Class2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using System.Media;
+using System.Collections;
+
+
+namespace kółko_i_krzyżyk
+{
+    class Class2 : btn_nowa_gra
+    {
+        public void Main()
+        {
+
+            string o;
+            string x;
+
+            o = wynik_o.Text;
+            x = wynik_x.Text;
+
+
+
+
+
+
+
+
+            
+            
+
+
+
+
+
+
+        }
+
+
+
+    }
+
+
+    }
+
